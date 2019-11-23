9a1cbecb0beb64ee7d04737405b821d8ae8794b0
John Jago <johnjago@protonmail.com>
2019-11-23 15:48:24 -0600

    Serve static files from /static/

--- router.go
+++ router.go
@@ -6,8 +6,13 @@ import (
 	"github.com/gorilla/mux"
 )
 
+const STATIC_DIR = "/static/"
+
 func NewRouter() *mux.Router {
 	router := mux.NewRouter().StrictSlash(true)
+	router.
+		PathPrefix(STATIC_DIR).
+		Handler(http.StripPrefix(STATIC_DIR, http.FileServer(http.Dir("."+STATIC_DIR))))
 	for _, route := range routes {
 		var handler http.Handler
 		handler = route.HandlerFunc
--- templates/index.go
+++ templates/index.go
@@ -7,6 +7,7 @@ const Index string = `<!DOCTYPE html>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Goupfile - Secure and anonymous file upload from the command line</title>
 	<link rel="stylesheet" href="https://unpkg.com/@bafs/mu@0.3/mu.min.css" />
+	<link rel="icon" type="image/png" href="/static/favicon-32x32.png" sizes="32x32">
 	<style>
 		body {
 			font-family: -apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,Oxygen-Sans,Ubuntu,Cantarell,"Helvetica Neue",sans-serif;
@@ -20,16 +21,6 @@ const Index string = `<!DOCTYPE html>
 	<h1>Goupfile <small>alpha</small></h1>
 	<p>Goupfile is secure and anonymous file upload from the command line. It allows you to share files and get a links for them without leaving your terminal.</p>
 	<h2>How does it work?</h2>
-	<p>You can use Goupfile through the <a href="https://github.com/goupfile/goup">goup CLI tool</a>.</p>
-	<p>For example:</p>
-	<pre>
-$ goup file.txt
-Uploading file...
-
-	https://goupfile.com/aw9kzm
-
-$ goup download aw9kzm
-</pre>
 	<p>The goup CLI tool is currently under development. You can try out Goupfile by sending an HTTP POST request with a multipart form body to upload a file. The key should be named 'file'.</p>
 	<pre>
 curl -F "file=@/home/user/file.txt" https://goupfile.com
@@ -37,6 +28,14 @@ curl -F "file=@/home/user/file.txt" https://goupfile.com
 	<p>To download a file:</p>
 <pre>
 curl https://goupfile.com/OVn1C1
+</pre>
+	<p>In the future, you will be able to use Goupfile through the <a href="https://github.com/goupfile/goup">goup CLI tool</a>.</p>
+	<p>For example:</p>
+	<pre>
+$ goup file.txt
+Uploading file...
+https://goupfile.com/aw9kzm
+$ goup download aw9kzm
 </pre>
 	<hr>
 	<a href="https://github.com/goupfile">GitHub</a>
