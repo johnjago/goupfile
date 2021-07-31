a14f79391edb32af6237429be0496d65580b9aa8
John Jago <johnjago@protonmail.com>
2021-07-31 12:34:49 -0500

    test: write a test for createURL

---
+++ handlers_test.go
@@ -0,0 +1,13 @@
+package main
+
+import "testing"
+
+func TestCreateURL(t *testing.T) {
+	id := "wcz20k"
+	action := "v"
+	want := "http://localhost:8090/v/wcz20k"
+	got := createURL(id, action)
+	if got != want {
+		t.Errorf("createURL(%s, %s) should produce %s, but was %s", id, action, want, got)
+	}
+}
